021b4d1851edffe9b697e66afe21e5a360653008
A.S.Rawat <adityasrwt5@gmail.com>
2023-01-11 13:45:03 +0530

    Fruit Cutter

--- Fruit-Cutter/Assets/Script/fruit.cs
+++ Fruit-Cutter/Assets/Script/fruit.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class fruit : MonoBehaviour
 {
     [SerializeField] private GameObject fruitSlicedPreFab;
+    [SerializeField] private float startForce = 15f;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Blade")
         {
-            Instantiate(fruitSlicedPreFab, transform.position, transform.rotation);
+            Vector3 direction = (col.transform.position - transform.position).normalized;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            GameObject sliceFruit = Instantiate(fruitSlicedPreFab, transform.position, rotation);
             Destroy(gameObject);
-
+            Destroy(sliceFruit, 5f);
         }
     }
 }
---
+++ Fruit-Cutter/Assets/Script/fruitSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruitSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject fruitPrefab;
+    [SerializeField] private Transform[] spawnPoints;
+    private float miniDelay = .1f;
+    private float maxDelay = 1f;
+    private void Start()
+    {
+        StartCoroutine(spawnFruits());
+    }
+
+    IEnumerator spawnFruits()
+    {
+        while (true)
+        {
+            float deplay = Random.Range(miniDelay, maxDelay);
+            yield return new WaitForSeconds(deplay);
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+            GameObject spawnFruit = Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnFruit, 5f);
+        }
+    }
+}
