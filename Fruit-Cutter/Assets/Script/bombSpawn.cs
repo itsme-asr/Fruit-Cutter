8c6aff285718b6d37633d1a7b7f5771309d5bff4
A.S.Rawat <adityasrwt5@gmail.com>
2023-01-13 20:36:22 +0530

    Fruit Cutter

--- Fruit-Cutter/Assets/Script/bombSpawn.cs
+++ Fruit-Cutter/Assets/Script/bombSpawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class bombSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject bombPrefab;
-    [SerializeField] private float bombDelay;
+    private float bombDelay;
     [SerializeField] private Transform[] spawnPoints;
     void Start()
     {
@@ -23,25 +23,8 @@ public class bombSpawn : MonoBehaviour
             Transform spawnPoint = spawnPoints[spawnIndex];
             GameObject bombTime = Instantiate(bombPrefab, spawnPoint.position, spawnPoint.rotation);
 
-            Debug.Log(bombDelay);
-            Debug.Log(GameObject.Find("Text").transform.GetComponent<FruitPoints>().ScorePoints);
-
-            int score = GameObject.Find("Text").transform.GetComponent<FruitPoints>().ScorePoints;
-            Debug.Log(score);
-
-            delayBomb(score);
-
             Destroy(bombTime, 5f);
         }
 
-    }
-
-    private void delayBomb(int score)
-    {
-        if (score == 75)
-        {
-            bombDelay = bombDelay - .1f;
-            Debug.Log(bombDelay);
-        }
     }
 }
